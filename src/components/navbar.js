46d4b0ff76192be61e7c0ca4dc1b30c084588926
Martin Mora <martinmora2778@gmail.com>
2022-07-02 18:29:30 -0300

    master

---
+++ src/components/navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./navbar.css"
+
+const  Navbar = () =>{
+  return (
+
+    <div className="Navbar">
+        <div class="nav-bg">
+        <nav class="nav-servicios enlaces">
+                <a href="../index.html">Inicio</a>
+                <a href="#">Contacto</a>
+                <a href="nosotros.html">Nosotros</a>
+        </nav>
+    </div>
+
+
+    </div>
+  )
+}
+
+export default Navbar
